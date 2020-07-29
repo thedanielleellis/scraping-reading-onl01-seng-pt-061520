cd52c5e0cfc458c3dfde16f3d4bd2e28694750cc
Danielle Ellis <thedanielleellis@gmail.com>
2020-07-29 18:10:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,9 @@ doc = Nokogiri::HTML(html)
  
 doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+doc.css(".headline-26OIBN")
+
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
  
 courses.each do |course|
